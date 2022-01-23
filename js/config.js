b58f5c024928fa62f6fffe51c5bc50c842089aac
THELUPA <32226207+thelupa1488@users.noreply.github.com>
2022-01-24 02:23:27 +0400

    rel

---
+++ js/config.js
@@ -0,0 +1,46 @@
+let config = {
+
+  // Задержка загрузки (1 секунда)
+  preloaderDelay: 1,
+
+  // Задержка появления видео (4.2 секунды)
+  showDelay: 7.7, 
+
+  // Заголовок
+  title: 'thelupa',
+
+  // Социальные сети
+  social: {
+    telegram: '@THELUPA1488',
+    steam: '76561198854331755',
+    vk: 'thelupa1488', 
+  },
+
+
+  // Цвет иконки загрузки
+  preloaderColor: '#f5323c',
+
+
+  // Цвет кнопки play
+  playButtonColor: '#f5323c',
+  // Цвет тени кнопки play
+  playButtonShadowColor: 'rgba(255, 0, 55, 5.0)',
+
+
+  // Цвет заголовка
+  titleColor: '#FF0000',
+  // Цвет тени загаловка
+  titleShadowColor: 'rgba(255, 0, 55, 0.6)',
+
+
+  // Текст копирайта
+  copyrightText: 'developer | thelupa',
+  // Цвет копирайта
+  copyrightTextColor: '#f5323c',
+
+  
+  // Цвет иконок социальных сетей
+  socialColor: '#FF0000',
+  // Цвет тени иконок социальных сетей
+  socialShadowColor: 'rgba(255, 255, 255, 0.2)',
+}
