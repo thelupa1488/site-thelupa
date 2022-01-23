634a9589698176b98bd5af367a5deb555c69507b
THELUPA <32226207+thelupa1488@users.noreply.github.com>
2022-01-24 02:59:36 +0400

    Update block.js

--- js/block.js
+++ js/block.js
@@ -1,4 +1,1 @@
-let url = 'err404.html';
-		window.addEventListener('devtoolschange', event => {
-				window.location = url;
-    });
+window.addEventListener('devtoolschange', event => { window.location = 'err404.html'; });
